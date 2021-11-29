39fb2adb85cda8316665d5dfe0f947364e494f5d
Pedro Motta <pedro.motta@avenuecode.com>
2021-11-29 19:14:53 -0300

    Removed unused import from exercise list screen

--- app/src/main/java/com/ch4k4uw/workout/egym/exercise/ExerciseListScreen.kt
+++ app/src/main/java/com/ch4k4uw/workout/egym/exercise/ExerciseListScreen.kt
@@ -1,7 +1,6 @@
 package com.ch4k4uw.workout.egym.exercise
 
 import android.content.res.Configuration
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
