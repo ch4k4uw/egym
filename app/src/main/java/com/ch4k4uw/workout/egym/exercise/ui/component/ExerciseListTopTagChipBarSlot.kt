9788ed76d34e3e29b1c2dab65ad43b35dcf7e791
Pedro Henrique Fatias Motta <garra_e@hotmail.com>
2021-12-03 07:04:13 +0000

    Merged in feature/chips-filter-appearance (pull request #7)
    Changed the chip filters panel surface color
    
    * Changed the chip filters panel surface color

--- app/src/main/java/com/ch4k4uw/workout/egym/exercise/ui/component/ExerciseListTopTagChipBarSlot.kt
+++ app/src/main/java/com/ch4k4uw/workout/egym/exercise/ui/component/ExerciseListTopTagChipBarSlot.kt
@@ -1,13 +1,13 @@
 package com.ch4k4uw.workout.egym.exercise.ui.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.AppBarDefaults
+import androidx.compose.material.Surface
 import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.ch4k4uw.workout.egym.common.ui.theme.EGymTheme
 import com.ch4k4uw.workout.egym.core.exercise.domain.data.ExerciseTag
 import com.ch4k4uw.workout.egym.core.ui.AppTheme
@@ -27,32 +27,37 @@ fun ExerciseListTopTagChipBarSlot(
     isLoadingStateForced: MutableState<Boolean>,
     onIntent: (ExerciseListIntent) -> Unit
 ) {
-    FlowRow(
-        modifier = modifier
-            .background(AppTheme.colors.material.primarySurface)
-            .padding(all = EGymTheme.Dimens.exerciseListTopTagChipBarSlot.padding),
-        mainAxisAlignment = MainAxisAlignment.Center,
-        mainAxisSize = SizeMode.Expand,
-        crossAxisSpacing = EGymTheme.Dimens.exerciseListTopTagChipBarSlot.crossAxisSpacing,
-        mainAxisSpacing = EGymTheme.Dimens.exerciseListTopTagChipBarSlot.mainAxisSpacing,
+    Surface(
+        modifier = modifier,
+        color = AppTheme.colors.material.primarySurface,
+        elevation = AppBarDefaults.TopAppBarElevation
     ) {
-        for (tag in exerciseTags) {
-            ExerciseListTagChip(tag = tag.first, isSelected = tag.second) { rawTag, state ->
-                exerciseTags
-                    .indexOfFirst { it.first == rawTag }
-                    .takeIf { it != -1 }
-                    ?.also { index ->
-                        showShimmer.value = true
-                        exercisesHeads.clear()
-                        isLoadingStateForced.value = true
-                        exerciseTags[index] = exerciseTags[index].copy(second = state)
-                        onIntent(
-                            ExerciseListIntent.PerformQuery(
-                                query = queryText.value,
-                                tags = exerciseTags.filter { it.second }.map { it.first }
+        FlowRow(
+            modifier = Modifier
+                .padding(all = EGymTheme.Dimens.exerciseListTopTagChipBarSlot.padding),
+            mainAxisAlignment = MainAxisAlignment.Center,
+            mainAxisSize = SizeMode.Expand,
+            crossAxisSpacing = EGymTheme.Dimens.exerciseListTopTagChipBarSlot.crossAxisSpacing,
+            mainAxisSpacing = EGymTheme.Dimens.exerciseListTopTagChipBarSlot.mainAxisSpacing,
+        ) {
+            for (tag in exerciseTags) {
+                ExerciseListTagChip(tag = tag.first, isSelected = tag.second) { rawTag, state ->
+                    exerciseTags
+                        .indexOfFirst { it.first == rawTag }
+                        .takeIf { it != -1 }
+                        ?.also { index ->
+                            showShimmer.value = true
+                            exercisesHeads.clear()
+                            isLoadingStateForced.value = true
+                            exerciseTags[index] = exerciseTags[index].copy(second = state)
+                            onIntent(
+                                ExerciseListIntent.PerformQuery(
+                                    query = queryText.value,
+                                    tags = exerciseTags.filter { it.second }.map { it.first }
+                                )
                             )
-                        )
-                    }
+                        }
+                }
             }
         }
     }
